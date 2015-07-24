e6dbe150602fd76c52e575744b52137003900802
balazon <balazon@gmail.com>
2015-07-24 10:15:48 +0200

    test structure done, and one test done

--- Tester.cpp
+++ Tester.cpp
@@ -11,6 +11,17 @@ Tester::~Tester()
 
 void Tester::InitTests()
 {
+	Test t1{ILines};
+	t1.A = 1.f;
+	t1.B = -2.f;
+	t1.C = -2.f;
+	t1.D = 1.f;
+	t1.E = 2.f;
+	t1.F = 8.f;
+	t1.feasible = true;
+	t1.x1 = 3.f;
+	t1.y1 = 2.5f;
+	AddTest(t1);
 	
 }
 
--- Tester.h
+++ Tester.h
@@ -2,8 +2,34 @@
 
 #include "MathUtils.h"
 
+bool IntersectLines(float A, float B, float C, float D, float E, float F, float& resX, float& resY);
+
+void OrthogonalProjectionOfPointOnLine(float A, float B, float C, float tx, float ty, float& resX, float& resY);
+
+bool QuadraticEquation(float a, float b, float c, float& x1, float& x2);
+
+bool IntersectLineCircle(float A, float B, float C, float u, float v, float r, float& x1, float& y1, float& x2, float& y2);
+
+bool IntersectCircleCircle(float u1, float v1, float r1, float u2, float v2, float r2, float& x1, float& y1, float& x2, float& y2);
+
+enum TestType {ILines, ProjectionPointLine, QuadEquation, ILineCircle, ICircleCircle};
+
 struct Test
 {
+	//test data
+	
+	TestType type;
+	//lines: Ax + By = C, or quadratic equations
+	float A, B, C, D, E, F;
+	//circles: (x-u1)^2 + (y-v1)^2 = r1^2
+	float u1, v1, r1, u2, v2, r2;
+	//a point
+	float px, py;
+	
+	//test's solution
+	bool feasible;
+	float x1, y1, x2, y2;
+	Test(TestType type);
 	
 };
 
